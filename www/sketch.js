6854b950fd450a8d3e6b084942fd7fb1293e0140
Kdev <kevinburchette@outlook.com>
2021-02-20 20:03:06 -0500

    added P5JS

--- www/sketch.js
+++ www/sketch.js
@@ -1,11 +1,24 @@
 let canvas;
+let s;
+let followX, followY;
+let size;
 
 function setup() {
   let canvas = createCanvas(displayWidth - (displayWidth * .15), displayHeight);
   canvas.parent('main');
+  s = 10
+  size = 50;
 }
 
 function draw() {
-  background(255);
-  rect(50, 50, 50, 50);
+  background(0);
+  if(mouseX >= 0) {
+    rect(mouseX, mouseY, size, size);
+  } else {
+    rect(s, 50, size, size);
+    if (s >= displayWidth) {
+      s = 0-size;
+    }
+  }
+  s += 10;
 }
